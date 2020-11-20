c6beb8a325585e2634bf0580d76ac5418675700b
Ittiporn094 <73686934+Ittiporn094@users.noreply.github.com>
2020-11-20 14:14:23 +0700

    Create update.php

---
+++ update.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once("connection.php");
+
+if(isset($_POST['update']))
+{
+    if(empty($_POST['name']) || empty($_POST['comment']) || empty($_POST['link']))
+    {
+        echo ' Please Fill in the Blanks ';
+    }
+    else
+    {
+        $UserID = $_GET['ID'];
+        $FixName = $_POST['name'];
+        $FixComment = $_POST['comment'];
+        $FixLink = $_POST['link'];
+
+        $query = "UPDATE guestbook SET NAME='".$FixName."', Comment='".$FixComment."', Link='".$FixLink."' where ID='".$UserID."'";
+        $result = mysqli_query($conn,$query);
+
+        if($result)
+        {
+            header("location:index.php");
+        }
+        else
+        {
+            echo '  Please Check Your Query ';
+        }
+    }
+}
+else
+{
+    header("location:index.php");
+}
+
+
+
+?>
