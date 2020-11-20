1266818d44dc54b5602c7cc5880d4352afd43071
Ittiporn094 <73686934+Ittiporn094@users.noreply.github.com>
2020-11-20 14:32:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
-<body class="bg-rgb(153, 153, 255)">
+<body class="bg-blue">
 <?php
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'ann123.mysql.database.azure.com', 'Ittiporn@ann123', 'OWUlhm78', 'itflab', 3306);
