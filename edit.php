b749cbd4a4b8f649165c5820c2fd97b755422a39
Ittiporn094 <73686934+Ittiporn094@users.noreply.github.com>
2020-11-20 14:36:33 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -16,7 +16,7 @@ $UserID = $_GET['GetID'];
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
-<body class="bg-dark">
+<body class="bg-Blue">
 <div class="container">
   <div class="row">
     <div class="col m-auto">
