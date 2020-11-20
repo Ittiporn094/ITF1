6a15f6652bbbbb4d6a8eafef05de61ac5bef17f2
Ittiporn094 <73686934+Ittiporn094@users.noreply.github.com>
2020-11-20 14:35:40 +0700

    Update add.php

--- add.php
+++ add.php
@@ -13,7 +13,7 @@ require_once("connection.php");
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
-<body class="bg-dark">
+<body class="bg-blue">
 <div class="container">
   <div class="row">
     <div class="col m-auto">
