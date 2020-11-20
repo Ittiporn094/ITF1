9976bf60d17996bf08f23cb49ebbe605d56a3173
Ittiporn094 <73686934+Ittiporn094@users.noreply.github.com>
2020-11-20 14:08:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,67 @@
 <html>
-  <head>
-    <title>จากวันนั้น</title>
-  </head>
-  <body>
-    <h1>นายอิทธิพร กุลจิตติวัฒนะ ชื่อเล่น อิท</h1>
-    <p>ก็ใช้ได้อยู่</p>
-    <img src="Pake.jpg">
-    <img src="ANN.jpg">
-    <img src="sarai.jpg">
-    <br>
-    <a href="https://www.youtube.com/watch?v=LACbVhgtx9I">Link song</a>
-    
-    <h2>เรื่องตลก</h2>
-    <h3>
-      คุณหมอบอกกับคนไข้ของเขาว่า “ผมมีข่าวไม่ดีเเละข่าวที่ไม่ดีกว่ามาบอก”
-      “เฮ้ยหมอ จริงดิ อย่าพูดให้ผมกลัวสิครับ ข่าวไม่ดีคืออะไร” ถามคนไข้
-      “คุณจะมีชีวิตอยู่อีกเเค่ 24 ชั่วโมงเท่านั้น” คุณหมอตอบ
-      “เฮ้ยจะบ้าไปเเล้วเหรอหมอ” คนไข้ร้อง “มันจะมีข่าวไหนที่เเย่ไปกว่านี้อีกหรือ”
-      คุณหมอจึงตอบไปว่า “ผมพยายามจะโทรบอกคุณตั้งเเต่เมื่อวานเเล้วเเต่โทรไม่ติด”
-    </h3>
-  </body>
+<head>
+<title>ITF Lab</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body class="bg-dark">
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'ann123.mysql.database.azure.com', 'Ittiporn@ann123', 'OWUlhm78', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+?>
+<div class="container">
+  <div class="row">
+    <div class="col m-auto>
+      <div class="col mt-5>
+        <table width="600" class="table rounded table-bordered">
+          <thead class="thead-light">
+          <tr class="bg-secondary">
+            <th width="25"> <div align="center">ID</div></th>
+            <th width="150"> <div align="center">Name</div></th>
+            <th width="300"> <div align="center">Comment </div></th>
+            <th width="175"> <div align="center">Link </div></th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+<?php
+          while($Result = mysqli_fetch_array($res))
+{
+           $UserID = $Result['ID'];
+           $Name = $Result['NAME'];
+           $Comment = $Result['Comment'];
+           $Link = $Result['Link'];
+?>
+           <tr class="bg-light">
+             <td><?php echo $UserID;?></div></td>
+             <td><?php echo $Name;?></div></td>
+             <td><?php echo $Comment;?></td>
+             <td><?php echo $Link;?></td>
+             <td><a href="edit.php?GetID=<?php echo $UserID ?>"><button type="button" class="btn btn-primary">Edit</button></a></td>
+             <td><a href="delete.php?GetID=<?php echo $UserID ?>"><button type="button" class="btn btn-danger">Delete</button></a></td>
+           </tr>
+<?php
+}
+?>
+          </thead>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+<div align="center">
+  <a href="add.php"><button type="button" class="btn btn-success">ADD</button></a>
+</div>
+<?php
+mysqli_close($conn);
+?>
+</body>
 </html>
